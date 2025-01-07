506117864b2c8c2c7811f91489b7a43146daa686
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2025-01-07 22:17:59 +0100

    corrected some lines of code

--- src/app/user-management/page.js
+++ src/app/user-management/page.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import AddNewUser from "@/components/add-new-user";
 
 function UserManagement() {
@@ -12,16 +11,3 @@ function UserManagement() {
     );
 }
    export default UserManagement;
-=======
-
-
-function UserManagement() {
-    return ( 
-        <div className="p-4">
-            <h1>User management</h1>
-        </div>
-     );
-}
-
-export default UserManagement;
->>>>>>> 5fe5a59f7df71b12eeb1b996bc2a73f56fcdfcda

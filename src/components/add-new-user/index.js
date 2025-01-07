d2f3a0d546e8dc1ae1e3ee98487a0dd8ce692ac0
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2025-01-07 20:31:25 +0100

    added some shadcn ui complement

---
+++ src/components/add-new-user/index.js
@@ -0,0 +1,21 @@
+'use client';
+import { Button } from "../ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+function AddNewUser() {
+    return (
+        <div>
+            <Button>Add New User</Button>
+        </div>
+    )
+}
+
+export default AddNewUser;
